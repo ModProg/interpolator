89b0f70fa918bfe10d0827c3b404a50436f9b341
Roland Fredenhagen <dev@modprog.de>
2023-02-22 02:28:43 +0100

    implement basic width

--- src/lib.rs
+++ src/lib.rs
@@ -29,39 +29,43 @@ pub struct Formattable<'a> {
 }
 
 macro_rules! formattable_fn {
-    ($name:ident<$($traits:ident),*> $($fields:ident),+) => {
+    ($name:ident, $builder:ident<$($traits:ident),*> $($fields:ident),+) => {
         pub fn $name<T: $($traits+)*>(value: &'a T) -> Self {
             Self {
                 $($fields: Some(value),)*
                 ..Default::default()
             }
         }
+        pub fn $builder<T: $($traits+)*>(mut self, value: &'a T) -> Self {
+            $(self.$fields = Some(value);)*
+            self
+        }
     };
-    ($name:ident<$($traits:ident),*>) => {
-        formattable_fn!($name <$($traits),*> $name);
+    ($name:ident, $builder:ident<$($traits:ident),*>) => {
+        formattable_fn!($name, $builder<$($traits),*> $name);
     }
 }
 macro_rules! formattable {
-    [$($name:ident<$($traits:ident),*> $($fields:ident),*;)*] => {
+    [$($name:ident, $builder:ident<$($traits:ident),*> $($fields:ident),*;)*] => {
         impl<'a> Formattable<'a> {
-            $(formattable_fn!($name<$($traits),*> $($fields),*);)*
+            $(formattable_fn!($name, $builder<$($traits),*> $($fields),*);)*
         }
     };
 }
 
 formattable![
-    debug_display<Debug, Display> debug, display;
-    debug<Debug>;
-    display<Display>;
-    number<Binary, Debug, Display, LowerExp, LowerHex, Octal, UpperExp, UpperHex>
+    debug_display, and_debug_display<Debug, Display> debug, display;
+    debug, and_debug<Debug>;
+    display, and_display<Display>;
+    number, and_number<Binary, Debug, Display, LowerExp, LowerHex, Octal, UpperExp, UpperHex>
         binary, debug, display, lower_exp, lower_hex, octal, upper_exp, upper_hex;
-    binary<Binary>;
-    lower_exp<LowerExp>;
-    lower_hex<LowerHex>;
-    octal<Octal>;
-    pointer<Pointer>;
-    upper_exp<UpperExp>;
-    upper_hex<UpperHex>;
+    binary, and_binary<Binary>;
+    lower_exp, and_lower_exp<LowerExp>;
+    lower_hex, and_lower_hex<LowerHex>;
+    octal, and_octal<Octal>;
+    pointer, and_pointer<Pointer>;
+    upper_exp, and_upper_exp<UpperExp>;
+    upper_hex, and_upper_hex<UpperHex>;
 ];
 
 pub fn format<K: Borrow<str> + Eq + Hash>(
@@ -84,6 +88,13 @@ pub fn format<K: Borrow<str> + Eq + Hash>(
             format = &format[1 + variable_len..];
             if format.starts_with(':') {
                 format = &format[1..];
+                let width_len = format.find(|c: char| !c.is_ascii_digit()).unwrap();
+                let mut width = 0;
+                if width_len != 0 {
+                    width = format[..width_len].parse().ok().unwrap();
+                    format = &format[width_len..];
+                };
+                dbg!(format);
                 match format.as_bytes()[0] {
                     b'#' => {
                         format = &format[1..];
@@ -91,22 +102,22 @@ pub fn format<K: Borrow<str> + Eq + Hash>(
                             write!(out, "{:#}", variable.display?).ok()?;
                         } else {
                             match format.as_bytes()[0] {
-                                b'?' => write!(out, "{:#?}", variable.debug?).ok()?,
-                                b'b' => write!(out, "{:#b}", variable.binary?).ok()?,
-                                b'e' => write!(out, "{:#e}", variable.lower_exp?).ok()?,
+                                b'?' => write!(out, "{:#width$?}", variable.debug?).ok()?,
+                                b'b' => write!(out, "{:#width$b}", variable.binary?).ok()?,
+                                b'e' => write!(out, "{:#width$e}", variable.lower_exp?).ok()?,
                                 b'x' if format.as_bytes()[1] == b'?' => {
                                     format = &format[1..];
-                                    write!(out, "{:#x?}", variable.debug?).ok()?
+                                    write!(out, "{:#width$x?}", variable.debug?).ok()?
                                 }
-                                b'x' => write!(out, "{:#x}", variable.lower_hex?).ok()?,
-                                b'o' => write!(out, "{:#o}", variable.octal?).ok()?,
-                                b'p' => write!(out, "{:#p}", variable.pointer?).ok()?,
-                                b'E' => write!(out, "{:#E}", variable.upper_exp?).ok()?,
+                                b'x' => write!(out, "{:#width$x}", variable.lower_hex?).ok()?,
+                                b'o' => write!(out, "{:#width$o}", variable.octal?).ok()?,
+                                b'p' => write!(out, "{:#width$p}", variable.pointer?).ok()?,
+                                b'E' => write!(out, "{:#width$E}", variable.upper_exp?).ok()?,
                                 b'X' if format.as_bytes()[1] == b'?' => {
                                     format = &format[1..];
-                                    write!(out, "{:#X?}", variable.debug?).ok()?
+                                    write!(out, "{:#width$X?}", variable.debug?).ok()?
                                 }
-                                b'X' => write!(out, "{:#X}", variable.upper_hex?).ok()?,
+                                b'X' => write!(out, "{:#width$X}", variable.upper_hex?).ok()?,
                                 _ => return None,
                             }
                             format = &format[1..];
@@ -114,22 +125,25 @@ pub fn format<K: Borrow<str> + Eq + Hash>(
                     }
                     b'x' if format.as_bytes()[1] == b'?' => {
                         format = &format[2..];
-                        write!(out, "{:x?}", variable.debug?).ok()?;
+                        write!(out, "{:width$x?}", variable.debug?).ok()?;
                     }
                     b'X' if format.as_bytes()[1] == b'?' => {
                         format = &format[2..];
-                        write!(out, "{:X?}", variable.debug?).ok()?;
+                        write!(out, "{:width$X?}", variable.debug?).ok()?;
+                    }
+                    b'}' => {
+                        write!(out, "{:width$}", variable.display?).ok()?;
                     }
                     b => {
                         match b {
-                            b'?' => write!(out, "{:?}", variable.debug?).ok()?,
-                            b'b' => write!(out, "{:b}", variable.binary?).ok()?,
-                            b'e' => write!(out, "{:e}", variable.lower_exp?).ok()?,
-                            b'x' => write!(out, "{:x}", variable.lower_hex?).ok()?,
-                            b'o' => write!(out, "{:o}", variable.octal?).ok()?,
-                            b'p' => write!(out, "{:p}", variable.pointer?).ok()?,
-                            b'E' => write!(out, "{:E}", variable.upper_exp?).ok()?,
-                            b'X' => write!(out, "{:X}", variable.upper_hex?).ok()?,
+                            b'?' => write!(out, "{:width$?}", variable.debug?).ok()?,
+                            b'b' => write!(out, "{:width$b}", variable.binary?).ok()?,
+                            b'e' => write!(out, "{:width$e}", variable.lower_exp?).ok()?,
+                            b'x' => write!(out, "{:width$x}", variable.lower_hex?).ok()?,
+                            b'o' => write!(out, "{:width$o}", variable.octal?).ok()?,
+                            b'p' => write!(out, "{:width$p}", variable.pointer?).ok()?,
+                            b'E' => write!(out, "{:width$E}", variable.upper_exp?).ok()?,
+                            b'X' => write!(out, "{:width$X}", variable.upper_hex?).ok()?,
                             _ => return None,
                         }
                         format = &format[1..];
@@ -156,7 +170,7 @@ fn test_format() {
     );
     assert_eq!(
         format(
-            "{hi} {hi:?} {int:#x?} {int:b} {int:#X} {int:o}",
+            "{hi:10} {hi:?} {int:#x?} {int:b} {int:#X} {int:4o}",
             [
                 ("hi", Formattable::debug_display(&"hello")),
                 ("int", Formattable::number(&123u8))
@@ -165,6 +179,6 @@ fn test_format() {
             .collect()
         )
         .unwrap(),
-        r#"hello "hello" 0x7b 1111011 0x7B 173"#
+        r#"hello      "hello" 0x7b 1111011 0x7B  173"#
     );
 }
