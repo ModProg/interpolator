4ef51202d1b2adee5618e0acdde25320696b13e5
Roland Fredenhagen <dev@modprog.de>
2023-02-23 19:16:45 +0100

    moved to validation tests disableling proptests simplifying

--- tests/test.rs
+++ tests/test.rs
@@ -1,9 +1,13 @@
+use std::f64::consts::PI;
 use std::fmt::Display;
+use std::iter;
 
 use derive_more::Display;
 use proptest::option;
 use proptest::prelude::*;
 use proptest::sample::select;
+use proptest::strategy::ValueTree;
+use proptest::test_runner::TestRunner;
 use proptest_derive::Arbitrary;
 
 #[derive(Debug, Arbitrary, Display, Clone)]
@@ -101,9 +105,97 @@ impl Display for FormatArgument {
     }
 }
 
+fn test(
+    value: impl Display,
+    converter: impl Display,
+    strategy: impl Strategy<Value = FormatArgument>,
+) {
+    let mut runner = TestRunner::default();
+    let values: Vec<_> = iter::repeat_with(|| strategy.new_tree(&mut runner))
+        .take(1000)
+        .map(|s| s.unwrap().current())
+        .map(|format_arg| {
+            let format_arg = format_arg.to_string();
+            let format_arg = format_arg.escape_default();
+            format!(r#"
+                assert_eq!(
+                    format("{format_arg}", [("ident", Formattable::{converter}(&{value}))].into_iter().collect()).unwrap(),
+                    format!("{format_arg}", ident = {value}),
+                    "{{}}", "{format_arg}"
+                );"#)
+        })
+        .collect();
+    let values = values.join("\n");
+    let t = trybuild2::TestCases::new();
+    t.pass_inline(&converter.to_string(), &format! {r#"
+        use template::{{format, Formattable}};
+        fn main() {{
+            {values}
+        }}
+    "#});
+}
+
+#[test]
+fn string() {
+    test(
+        "\"test\"",
+        "debug_display",
+        FormatArgument::arbitrary_with(&[
+            Trait::Display,
+            Trait::Question,
+            Trait::XQuestion,
+            Trait::xQuestion,
+        ]),
+    );
+}
+
+#[test]
+fn integer() {
+    test(
+        42,
+        "integer",
+        FormatArgument::arbitrary_with(&[
+            Trait::Display,
+            Trait::Question,
+            Trait::XQuestion,
+            Trait::xQuestion,
+            Trait::x,
+            Trait::X,
+            Trait::b,
+            Trait::o,
+            Trait::e,
+            Trait::E,
+        ]),
+    );
+}
+#[test]
+fn float() {
+    test(
+        PI,
+        "float",
+        FormatArgument::arbitrary_with(&[
+            Trait::Display,
+            Trait::Question,
+            Trait::XQuestion,
+            Trait::xQuestion,
+            Trait::e,
+            Trait::E,
+        ]),
+    );
+}
+#[test]
+fn pointer() {
+    test(
+        "&42",
+        "pointer",
+        FormatArgument::arbitrary_with(&[Trait::p]),
+    );
+}
+
 proptest! {
     #[test]
-    fn string(format_arg in FormatArgument::arbitrary_with(&[Trait::Display, Trait::Question, Trait::XQuestion, Trait::xQuestion])) {
+    #[ignore = "long runtime"]
+    fn string_pt(format_arg in FormatArgument::arbitrary_with(&[Trait::Display, Trait::Question, Trait::XQuestion, Trait::xQuestion])) {
         let t = trybuild2::TestCases::new();
         let format_arg = format_arg.to_string();
         let format_arg = format_arg.escape_default();
@@ -125,7 +217,8 @@ proptest! {
     }
 
     #[test]
-    fn integer(format_arg in FormatArgument::arbitrary_with(&[
+    #[ignore = "long runtime"]
+    fn integer_pt(format_arg in FormatArgument::arbitrary_with(&[
             Trait::Display, Trait::Question, Trait::XQuestion, Trait::xQuestion,
             Trait::x, Trait::X, Trait::b, Trait::o, Trait::e, Trait::E
     ])) {
@@ -145,7 +238,8 @@ proptest! {
     }
 
     #[test]
-    fn float(format_arg in FormatArgument::arbitrary_with(&[
+    #[ignore = "long runtime"]
+    fn float_pt(format_arg in FormatArgument::arbitrary_with(&[
             Trait::Display, Trait::Question, Trait::XQuestion, Trait::xQuestion, Trait::e, Trait::E
     ])) {
         let t = trybuild2::TestCases::new();
@@ -164,7 +258,8 @@ proptest! {
     }
 
     #[test]
-    fn pointer(format_arg in FormatArgument::arbitrary_with(&[Trait::p])) {
+    #[ignore = "long runtime"]
+    fn pointer_pt(format_arg in FormatArgument::arbitrary_with(&[Trait::p])) {
         let t = trybuild2::TestCases::new();
         let format_arg = format_arg.to_string();
         let format_arg = format_arg.escape_default();
