35562bebbd7d5fdc8d81ceacbc7743b147a7450c
Roland Fredenhagen <dev@modprog.de>
2023-02-23 19:30:21 +0100

    reduce test count

--- tests/test.rs
+++ tests/test.rs
@@ -112,7 +112,7 @@ fn test(
 ) {
     let mut runner = TestRunner::deterministic();
     let values: Vec<_> = iter::repeat_with(|| strategy.new_tree(&mut runner))
-        .take(1000)
+        .take(10)
         .map(|s| s.unwrap().current())
         .map(|format_arg| {
             let format_arg = format_arg.to_string();
