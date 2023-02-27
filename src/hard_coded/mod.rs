cf0cf3d3c5c7ebe575bb9ce97687fdcbc25b8411
Roland Fredenhagen <dev@modprog.de>
2023-02-27 15:12:01 +0100

    cleanup naming

--- src/hard_coded/mod.rs
+++ src/hard_coded/mod.rs
@@ -53,26 +53,26 @@ macro_rules! call_format_value {
 }
 
 #[cfg(feature = "number")]
+mod binary;
+#[cfg(feature = "debug")]
+mod debug;
+mod display;
+#[cfg(feature = "number")]
+mod lower_exp;
+#[cfg(feature = "number")]
+mod lower_hex;
+#[cfg(feature = "debug")]
+mod lower_hex_question;
+#[cfg(feature = "number")]
+mod octal;
+#[cfg(feature = "pointer")]
+mod pointer;
+#[cfg(feature = "number")]
 mod upper_exp;
 #[cfg(feature = "number")]
 mod upper_hex;
 #[cfg(feature = "debug")]
 mod upper_hex_debug;
-#[cfg(feature = "number")]
-mod binary;
-mod display;
-#[cfg(feature = "number")]
-mod lower_exp;
-#[cfg(feature = "number")]
-mod octal;
-#[cfg(feature = "pointer")]
-mod pointer;
-#[cfg(feature = "debug")]
-mod debug;
-#[cfg(feature = "number")]
-mod lower_hex;
-#[cfg(feature = "debug")]
-mod lower_hex_question;
 
 #[allow(clippy::too_many_arguments)]
 pub(crate) fn format_value(
@@ -91,15 +91,15 @@ pub(crate) fn format_value(
         match out, value, width, precision, alignment, sign, hash, zero, trait_, idx {
             Display => display(get_display),
             Question => debug(get_debug, "debug"),
-            xQuestion => lower_hex_question(get_debug, "debug"),
-            XQuestion => upper_hex_debug(get_debug, "debug"),
-            x => lower_hex(get_lower_hex, "number"),
-            X => upper_hex(get_upper_hex, "number"),
-            b => binary(get_binary, "number"),
-            o => octal(get_octal, "number"),
-            e => lower_exp(get_lower_exp, "number"),
-            E => upper_exp(get_upper_exp, "number"),
-            p => pointer(get_pointer, "pointer"),
+            LowerHexQuestion => lower_hex_question(get_debug, "debug"),
+            UpperHexQuestion => upper_hex_debug(get_debug, "debug"),
+            LowerHex => lower_hex(get_lower_hex, "number"),
+            UpperHex => upper_hex(get_upper_hex, "number"),
+            Binary => binary(get_binary, "number"),
+            Octal => octal(get_octal, "number"),
+            LowerExp => lower_exp(get_lower_exp, "number"),
+            UpperExp => upper_exp(get_upper_exp, "number"),
+            Pointer => pointer(get_pointer, "pointer"),
         }
     }
     .map_err(|e| Error::FmtError(e, idx))
